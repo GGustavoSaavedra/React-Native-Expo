3514dc41f0121090123fb123814aecb5ed2ae19a
GustavoSaavedra <gustavo9418@gmail.com>
2025-10-08 15:29:13 -0300

    Subida inicial del curso

---
+++ 01-bases/src/App.tsx
@@ -0,0 +1,15 @@
+// import { BasicTypes } from "./typescript/BasicTypes";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React + TS</h1>
+
+      {/* <BasicTypes /> */}
+    </>
+  );
+}
+
+export default App;
