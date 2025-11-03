97db4e245d4c281909dd59a1a93eea989590a6b1
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-03 11:57:40 -0300

    S5: Inicio proyecto

---
+++ 04-calculator-app/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+
+      <Slot />
+    </View>
+  );
+};
+
+export default RootLayout;
