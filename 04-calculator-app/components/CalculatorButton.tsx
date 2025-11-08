1e58f66f81966efaac9dd0fe69cd894c1916784d
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-08 16:46:39 -0300

    S5: Calculator buttons P2

--- 04-calculator-app/components/CalculatorButton.tsx
+++ 04-calculator-app/components/CalculatorButton.tsx
@@ -6,6 +6,7 @@ interface Props {
   label: string;
   color?: string;
   blackText?: boolean;
+  doubleSize?: boolean;
   onPress: () => void;
 }
 
@@ -13,10 +14,19 @@ export const CalculatorButton = ({
   label,
   color = Colors.darkGray,
   blackText = false,
+  doubleSize = false,
   onPress,
 }: Props) => {
   return (
-    <Pressable style={globalStyles.button} onPress={onPress}>
+    <Pressable
+      style={({ pressed }) => ({
+        ...globalStyles.button,
+        backgroundColor: color,
+        opacity: pressed ? 0.8 : 1,
+        width: doubleSize ? 180 : 80,
+      })}
+      onPress={onPress}
+    >
       <Text
         style={{
           ...globalStyles.buttonText,
