7a395ed01a9f1ac70c2e27b86d3ee6bbbf7f70a5
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-08 16:26:14 -0300

    S5: Calculator buttons

--- 04-calculator-app/app/index.tsx
+++ 04-calculator-app/app/index.tsx
@@ -1,12 +1,42 @@
+import { CalculatorButton } from "@/components/CalculatorButton";
 import { ThemeText } from "@/components/ThemeText";
+import { Colors } from "@/constants/Colors";
 import { globalStyles } from "@/styles/global-styles";
 import { View } from "react-native";
 
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <ThemeText variant="h1">50 x 50</ThemeText>
-      <ThemeText variant="h2">250</ThemeText>
+      {/* Resultados */}
+
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText variant="h1">50 x 50</ThemeText>
+        <ThemeText variant="h2">250</ThemeText>
+      </View>
+
+      {/* Filas de botones */}
+
+      <View style={globalStyles.row}>
+        <CalculatorButton
+          label="C"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("C")}
+        />
+        <CalculatorButton
+          label="+/-"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("+/-")}
+        />
+        <CalculatorButton
+          label="del"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("del")}
+        />
+        <CalculatorButton label="÷" onPress={() => console.log("÷")} />
+      </View>
     </View>
   );
 };
