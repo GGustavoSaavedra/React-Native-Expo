aa0b681598b4ae24dbd66d9dacc1f1f5c2b0b554
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-19 17:22:21 -0300

    S6: Custom fonts in Nativewind

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -1,10 +1,16 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">Hola mundo</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-3xl">Hola mundo</Text>
+
+        <Text className="text-3xl font-work-black">Hola mundo</Text>
+        <Text className="text-3xl ">Hola mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
