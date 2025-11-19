bcc987aaf3e42c89c9cd54abb4d39b2493fe7be9
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-19 17:01:56 -0300

    S6: Instalation Nativewind

---
+++ 05-navigation-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-3xl font-bold text-blue-500">Hola mundo</Text>
+    </View>
+  );
+};
+
+export default App;
