5a1eb231123bbd22695e74b32deaa36e6b0723cd
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-20 12:47:36 -0300

    S6: Customizing StackNavigation

--- 05-navigation-app/app/(stack)/_layout.tsx
+++ 05-navigation-app/app/(stack)/_layout.tsx
@@ -2,7 +2,14 @@ import { Stack } from "expo-router";
 
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
       <Stack.Screen
         name="home/index"
         options={{
