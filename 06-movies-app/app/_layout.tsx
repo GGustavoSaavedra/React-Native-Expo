96b2b13ee2445748fdea4afd24e9eb313249e64d
GustavoSaavedra <gustavo9418@gmail.com>
2025-11-28 18:31:15 -0300

    S8: TanStack Query - Asynchronous State Manager

--- 06-movies-app/app/_layout.tsx
+++ 06-movies-app/app/_layout.tsx
@@ -1,12 +1,20 @@
-import { Text, View } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { Stack } from "expo-router";
+import React from "react";
 import "../global.css";
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text className="text-3xl">RootLayout</Text>
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </QueryClientProvider>
   );
 };
 
